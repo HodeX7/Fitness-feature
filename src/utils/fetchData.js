f5905bf792d51046d19f61a7ba7c4a31e9322ecf
Akshar Mehta <aksharmehta@Akshars-MacBook-Air.local>
2022-08-13 23:42:36 +0530

    Basic Boilerplate + API calls

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "a5e8c9e2dbmsh634f1a7d13011a1p10b40ajsnb7f5e8a9f1f0",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+};
